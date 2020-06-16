8f83d3cd711d3bfb47695e017fb3dbe49fd9d36e
contrapunctus <music.kashish@gmail.com>
2020-06-17 01:23:03 +0530

    Create basic loop and make-process call

--- rsync.el
+++ rsync.el
@@ -20,16 +20,39 @@
 ;;; Code:
 
 (defvar rsync-directory-alists nil
-  "Alist of directories to be synced, in the form (\"souce\" . \"destination\").")
+  "Alist of directories to be synced, in the form (\"SOURCE\" . \"DESTINATION\").
+SOURCE will be appended to DESTINATION, so the destination path
+will be DESTINATION/SOURCE/.")
 
 (defvar rsync-command "rsync"
   "Name of command to run. Can also be a path to the binary.")
 
-(defvar rsync-common-options '("-A" "-H" "-P" "-X" "-a" "-h" "-s" "x" "--checksum" "--delete-after")
+;; -P creates a progress bar. It is a nuisance at this stage, but maybe we can make a better progress bar using it?
+(defvar rsync-arguments '("-A" "-H" "-P" "-X" "-a" "-h" "-s" "x"
+                     "--checksum" "--delete-after")
   "List of options to be used in all calls to rsync.
 It is not designed to contain \"-n\"/\"--dry-run\".")
 
-;; (make-process )
+(defun rsync-sentinel (proc event)
+  (message "%s %s" proc event))
+
+(defun rsync ()
+  "Run rsync (with \"--dry-run\") for each pair of paths in `rsync-directory-alists'.
+Display the rsync output in a buffer. The user may inspect the
+output, and possibly accept it, which will run the same rsync
+command again, but without the \"--dry-run.\""
+  (cl-loop for (source . destination) in rsync-directory-alists do
+    (let ((source      (expand-file-name source))
+          (destination (expand-file-name destination)))
+      (make-process :name "rsync"
+                    :buffer (generate-new-buffer-name "rsync-output")
+                    :command `(,rsync-command
+                               ,@rsync-arguments
+                               "--dry-run"
+                               ,source
+                               ,(concat destination source))
+                    :connection-type 'pipe
+                    :stderr "rsync-errors"))))
 
 (provide 'rsync)
 
